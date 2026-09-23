c873a854d02a0b069ce506d4cbc30d1832bb5e4b
sabbir rahman <sr492033@gmail.com>
2026-09-24 00:42:38 +0600

    Fix typo in prompt for input value b

--- add two numbers.c
+++ add two numbers.c
@@ -5,7 +5,7 @@ int main() {
  float a,b,sum;
     printf("enter the value of a:");
     scanf("%f",&a);
-    printf("rnter the value if b:");
+    printf("enter the value if b:");
     scanf("%f",&b);
     sum=a+b;
     printf("The sum of two value:%.3f",sum);
