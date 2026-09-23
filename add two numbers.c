c08eae36cb82f536b05e40a553d9ec1fc6276c5f
sabbir rahman <sr492033@gmail.com>
2026-09-24 00:22:13 +0600

    Add program to sum two floating-point numbers

---
+++ add two numbers.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main() {
+ float a,b,sum;
+    printf("enter the value of a:");
+    scanf("%f",&a);
+    printf("rnter the value if b:");
+    scanf("%f",&b);
+    sum=a+b;
+    printf("The sum of two value:%.3f",sum);
+    
+    return 0;
+}
