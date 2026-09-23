171050828b7d62ab9079fad65858534695e3a2ae
sabbir rahman <sr492033@gmail.com>
2026-09-24 00:42:24 +0600

    Fix typo in prompt for input b

--- divide two numbers.c
+++ divide two numbers.c
@@ -5,7 +5,7 @@ int main() {
  float a,b,Result;
     printf("enter the value of a:");
     scanf("%f",&a);
-    printf("rnter the value if b:");
+    printf("enter the value if b:");
     scanf("%f",&b);
     Result=a/b;
     printf("The divide of two value:%.3f",Result);
